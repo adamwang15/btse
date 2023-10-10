9c19ac360425ae650bbdcb3492a238ce371c95ae
adamwang15 <adamwang15@gmail.com>
2023-10-10 12:15:53 +1100

    docs: update bsvar document

--- R/bvar.R
+++ R/bvar.R
@@ -5,7 +5,7 @@
 #' @param Y dependent variables, n by m
 #' @param p number of lags
 #' @param S number of draws
-#' @param prior list of priors \eqn{(B_0,V_0,\Lambda_0,\nu_0)}
+#' @param prior list of priors
 #'
 #' @export
 bvar <- function(Y, p, S, prior) {
@@ -25,14 +25,36 @@ bvar <- function(Y, p, S, prior) {
   .bvar_cpp(as.matrix(Y), p, S, prior)
 }
 
+
 #' Bayesian Structural vector autoregression
 #'
-#' Bayesian Structural vector autoregression
+#' Estimate a Bayesian Structural vector autoregression, base on the VAR(p) specification:
+#' \deqn{y_t = A_1 y_{t-1} + \cdots + A_p y_{t-p} + u_t}
+#' where \eqn{y_t} and \eqn{u_t} are \eqn{m \times 1}, \eqn{A_j} is \eqn{m \times m}.
+#' We can stack the \eqn{A_j}'s vertically
+#' such that \eqn{A=(A_1,\dots,A_p)'} which is \eqn{mp \times m}, and \eqn{y_{t-j}}'s vertically
+#' such that \eqn{x_t=(y_{t-1}',\cdots,y_{t-p}')'} which is \eqn{mp \times 1}. We have
+#' \deqn{y_t = A' x_t + u_t, \quad Y = XA + U}
+#' where \eqn{Y/U} are vertically stacked \eqn{y_t'/u_t'} and \eqn{n \times m},
+#' \eqn{X} is vertically stacked \eqn{x_t'} and \eqn{n \times mp}. \cr
+#' Now the conjugate homoskedastic Normal-Inverse-Wishart prior is
+#' \eqn{u_t|\Sigma\sim MN(0,I,\Sigma)} and
+#' \deqn{Y|A,\Sigma,X\sim MN(XA,I,\Sigma),\quad
+#' A|\Sigma,X\sim MN(A_0,V_0,\Sigma),\quad
+#' \Sigma|X\sim IW(\Lambda_0,\nu_0)}
+#' For SVAR, the forecast errors \eqn{u_t} can be decomposed to structural shocks \eqn{e_t}
+#' such that
+#' \deqn{u_t = P Q e_t}
+#' where \eqn{P,Q} are \eqn{m \times m}, \eqn{P=\text{chol}(\Sigma)} and
+#' \eqn{Q} is some orthogonal matrix (usually just the identity matrix \eqn{I}) since
+#' \deqn{\text{var}(PQe_t)=PQQ'P'=PP'=\Sigma=\text{var}(u_t) \Rightarrow P=\text{chol}(\Sigma)}
+#' Then, the model can be compactly written as
+#' \deqn{y_t = A' x_t + P Q e_t}
 #'
-#' @param Y dependent variables, n by m
+#' @param Y variables, n by m, n observations/periods, m variables
 #' @param p number of lags
 #' @param S number of draws
-#' @param prior list of priors \eqn{(B_0,V_0,\Lambda_0,\nu_0)}
+#' @param prior list of priors \eqn{(A_0,V_0,\Lambda_0,\nu_0)}
 #'
 #' @export
 bsvar <- function(Y, p, S, prior, identification, sign = NULL) {
